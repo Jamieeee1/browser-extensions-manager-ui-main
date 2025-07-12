7685e4f94ee0ae09168ded1bc94748fbea3c149b
Barnabas James <barnabasjames27@gmail.com>
2025-07-12 18:31:31 +0100

    kkl

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./app.jsx";
 import { DataContext, DataProvider } from "./app.jsx";
 
 createRoot(document.getElementById("root")).render(
