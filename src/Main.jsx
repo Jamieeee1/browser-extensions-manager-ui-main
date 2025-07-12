b457148252481fce43826a5f8b1299837f8b29b5
Barnabas James <barnabasjames27@gmail.com>
2025-07-12 18:26:27 +0100

    lmaoo

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import { DataContext, DataProvider } from "./app";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
